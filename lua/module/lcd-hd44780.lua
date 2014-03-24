e50032c12217e891a18ec3f74ac690187577f48f
Thijs Schreijer <thijs@thijsschreijer.nl>
2014-03-24 18:08:07 +0100

    several fixes

--- lua/module/lcd-hd44780.lua
+++ lua/module/lcd-hd44780.lua
@@ -191,7 +191,7 @@ end
 -- On first call it will replace itself with a new implementation based on LuaSocket
 -- or the OS. 
 function M.delayMicroseconds(microseconds)
-  local sleep = (package.loaded("socket") or {}).sleep
+  local sleep = (package.loaded.socket or {}).sleep
   if sleep then
     -- use LuaSocket sleep function
     M.delayMicroseconds = function(microseconds)
@@ -243,6 +243,8 @@ function M.initialize(pin_rs, pin_e, pin_db)
   self.rightToLeft = rightToLeft
   self.autoScroll = autoScroll
   self.noAutoScroll = noAutoScroll
+  self.pulseEnable = pulseEnable
+  self.message = message
 
   write4bits(self, 0x33) -- initialization
   write4bits(self, 0x32) -- initialization
